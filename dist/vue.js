dba27cb35031ad635a6ee73be51e0ba99eddbacd
aoxiang9696 <aoxiang9696@163.com>
2020-08-02 15:45:46 +0800

    8.2

--- dist/vue.js
+++ dist/vue.js
@@ -61,7 +61,7 @@
   var _toString = Object.prototype.toString;
 
   function toRawType (value) {
-    return _toString.call(value).slice(8, -1)
+    return _toString.call(value).slice(8, -1)  // 获取类属性
   }
 
   /**
